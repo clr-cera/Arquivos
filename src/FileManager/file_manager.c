7ae171945ea02eb4e0b73cfc594bdeb5be7085de
clr <clrcera05@gmail.com>
2024-04-01 11:30:39 -0300

    renames for cohesion

--- src/FileManager/file_manager.c
+++ src/FileManager/file_manager.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void insert_name_in_registry(FileManager fm, string file_name);
-void create_file(FileManager fm, string file_name);
-void delete_file(FileManager fm, string file_name);
+void fm_insert_name_in_registry(FileManager fm, string file_name);
+void fm_create_file(FileManager fm, string file_name);
+void fm_delete_file(FileManager fm, string file_name);
 
 typedef struct file_manager_obj_ {
   string* file_name_registry;
@@ -27,7 +27,7 @@ void erase_file_manager(FileManager* fmp) {
 
   for(int i = 0; i < fm->file_number; i++)
     if (fm->file_name_registry[i] != NULL) {
-      delete_file(fm, fm->file_name_registry[i]);
+      fm_delete_file(fm, fm->file_name_registry[i]);
     }
 
   for(int i = 0; i < fm->file_number; i++) 
@@ -42,25 +42,25 @@ void erase_file_manager(FileManager* fmp) {
 }
 
 void fm_create_empty_table(FileManager fm, string file_name) {
-  insert_name_in_registry(fm, file_name);
-  create_file(fm, file_name);
+  fm_insert_name_in_registry(fm, file_name);
+  fm_create_file(fm, file_name);
 }
 
-void create_file(FileManager fm, string file_name) {
+void fm_create_file(FileManager fm, string file_name) {
   string file_path = concat_string(DATA_PATH, file_name);
 
   fclose(fopen(file_path, "w")); 
   free(file_path);
 }
 
-void delete_file(FileManager fm, string file_name) {
+void fm_delete_file(FileManager fm, string file_name) {
   string file_path = concat_string(DATA_PATH, file_name);
   
   remove(file_path);
   free(file_path);
 }
 
-void insert_name_in_registry(FileManager fm, string file_name) {
+void fm_insert_name_in_registry(FileManager fm, string file_name) {
   if (fm->file_number != 0 && (fm->file_number % 5) == 0) {
     fm->file_name_registry = (string *) realloc(fm->file_name_registry,(fm->file_number+5) * sizeof(string));
   }
