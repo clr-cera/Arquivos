dd44847d7e006a4ba2b210eec172cc752ea365d8
clr <clrcera05@gmail.com>
2024-04-01 15:12:15 -0300

    initial header is done

--- src/FileManager/FileWalker/file_walker.c
+++ src/FileManager/FileWalker/file_walker.c
@@ -6,17 +6,21 @@
 
 typedef struct file_walker_ {
   string file_path;
+  FILE* fp;
   Header header;
 
 }file_walker_obj;
 typedef file_walker_obj* FileWalker;
 
 
-FileWalker create_file_walker(string file_path) {
+FileWalker create_file_walker(string file_path, bool load_header) {
   FileWalker fw = (FileWalker) malloc(sizeof(file_walker_obj));
+  fw->fp = fopen(file_path, "rb+");
   
   fw->file_path = file_path;
-  fw->header = NULL;
+
+  if (load_header) fw->header = get_header(fw->fp);
+  else fw->header = new_header();
 
   return fw;
 }
--- src/FileManager/FileWalker/file_walker.h
+++ src/FileManager/FileWalker/file_walker.h
@@ -1,7 +1,8 @@
 // file_walker é responsável por todas operações internas a um arquivo
 #include "../../Lib/lib.h"
+#include <stdbool.h>
 
 typedef struct file_walker_* FileWalker;
 
-FileWalker create_file_walker(string file_path);
+FileWalker create_file_walker(string file_path, bool new_header);
 
