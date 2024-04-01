52e209b0979488e1f35c846acecd05b189e3e388
clr <clrcera05@gmail.com>
2024-04-01 15:44:55 -0300

    write register

--- src/FileManager/FileWalker/Register/register.c
+++ src/FileManager/FileWalker/Register/register.c
@@ -1,7 +1,7 @@
 #include "register.h"
 #include <stdlib.h>
 
-
+// Todos os tam não devem contar o \0 da string
 typedef struct register_{
   char removido;
   int tamanhoRegistro;
@@ -16,7 +16,7 @@ typedef struct register_{
   int tamNacionalidade;
   string nacionalidade;
   
-  int tamNomeClub;
+  int tamNomeClube;
   string nomeClube;
 }register_obj;
 typedef register_obj* Register;
@@ -26,3 +26,21 @@ typedef struct register_collection_ {
   int length;
 }register_collection;
 
+
+
+void write_register(FILE* fp, Register reg) {
+  fwrite(&(reg->removido), sizeof(char), 1, fp);
+  fwrite(&(reg->tamanhoRegistro), sizeof(int), 1, fp);
+  fwrite(&(reg->prox), sizeof(long int), 1, fp);
+  fwrite(&(reg->id), sizeof(int), 1, fp);
+  fwrite(&(reg->idade), sizeof(int), 1, fp);
+
+  fwrite(&(reg->tamNomeJog), sizeof(int), 1, fp);
+  fwrite(reg->nomeJogador, sizeof(char), reg->tamNomeJog, fp); 
+
+  fwrite(&(reg->tamNacionalidade), sizeof(int), 1, fp);
+  fwrite(reg->nacionalidade, sizeof(char), reg->tamNacionalidade, fp); 
+  
+  fwrite(&(reg->tamNomeClube), sizeof(int), 1, fp);
+  fwrite(reg->nomeClube, sizeof(char), reg->tamNomeClube, fp); 
+}
