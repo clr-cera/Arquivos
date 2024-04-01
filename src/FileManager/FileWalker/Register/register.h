94b49dcb5101a1ad3613a0436111aa35a2b2c375
clr <clrcera05@gmail.com>
2024-04-01 15:28:26 -0300

    fix

--- src/FileManager/FileWalker/Register/register.h
+++ src/FileManager/FileWalker/Register/register.h
@@ -6,4 +6,4 @@ typedef struct register_* Register;
 typedef struct register_collection_ register_collection;
 
 Register create_empty_register(void);
-Register* csv_to_register_vector(string file_path);
+register_collection csv_to_register_vector(string file_path);
