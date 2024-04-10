9c0a6415e07dde0b63e1a661acabb27211c4490e
clr <clrcera05@gmail.com>
2024-04-09 22:36:26 -0300

    now there is no file left

--- src/FileManager/file_manager.h
+++ src/FileManager/file_manager.h
@@ -14,3 +14,6 @@ int fm_insert_csv(FileManager fm, string file_name, string csv_path);
 
 int fm_print_all(FileManager fm, string file_name);
 int fm_print_all_filter(FileManager fm, string file_name, Filter filter);
+
+void fm_delete_file(FileManager fm, string file_name);
+
--- src/Operations/create_table.c
+++ src/Operations/create_table.c
@@ -6,9 +6,14 @@ int create_table(string input_path, string output_name, FileManager fm) {
   fm_create_empty_table(fm, output_name);
   int returnal = fm_insert_csv(fm, output_name, input_path);
 
-  string binario_path = concat_string(DATA_PATH, output_name); 
-  binarioNaTela(binario_path);
-  free(binario_path);
+  if (returnal != -1) {
+    string binario_path = concat_string(DATA_PATH, output_name); 
+    binarioNaTela(binario_path);
+    free(binario_path);
+  }
+  else{
+    fm_delete_file(fm, output_name);
+  }
 
   return returnal;
 }
